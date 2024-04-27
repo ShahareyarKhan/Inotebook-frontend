09f68e8b6f6391a0a395282e792c11e2aa58c34d
ShahareyarKhan <shahareyar2003@gmail.com>
2024-04-28 00:01:04 +0530

    commit

--- src/Components/Both.js
+++ src/Components/Both.js
@@ -7,7 +7,7 @@ const Both = () => {
     return (
         <div className=' h-[100vh] flex items-center justify-center'>
 
-            <div className=' flex flex-col  w-full lg:w-1/3 border-[2px] rounded-[10px] border-black toggle box '>
+            <div className=' flex flex-col  w-full mx-2 md:w-2/3 lg:w-1/3 border-[2px] rounded-[10px] border-black toggle box '>
 
                 <div className='flex bg-[#ff9719] rounded-full'>
                     <div className={`w-1/2 flex justify-center p-5 text-xl font-semibold ${login === true ? "bg-white rounded-l-[9px]" : "bg-inherit rounded-[9px]"} toggle`} onClick={() => setlogin(true)} >Login</div>
